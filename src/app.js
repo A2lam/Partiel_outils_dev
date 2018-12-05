6c5ebf13369cff9b73075bb0eeb2757f34faaf38
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2018-12-05 16:26:13 +0100

    Installation de nodemon pour un tournage du serveur plus efficace en dév

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
+const data = require('./data/data');
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => res.json({}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send(data);
+});
 
 app.listen(process.env.PORT, () => console.log('Listening...'));
 
